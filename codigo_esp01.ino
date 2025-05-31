c8f3f3b5608a0e6ee6955cf5eafba265f80ef9a1
Lucas-Vitorino <lucasvitorino12@gmail.com>
2025-05-31 17:16:33 -0300

    codigo esp01

---
+++ codigo_esp01.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "YASMIN_E_LUCAS_2.4G";
+const char* senha = "Judo@202353";
+const String host = "https://projetoarduino.onrender.com/dados";
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, senha);
+  Serial.print("Conectando ao Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\n✅ Wi-Fi conectado.");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String temp = Serial.readStringUntil('\n');
+    temp.trim();
+
+    if (WiFi.status() == WL_CONNECTED) {
+      WiFiClientSecure client;
+      client.setInsecure(); // ignora verificação SSL (funciona com HTTPS do Render)
+
+      HTTPClient http;
+      String url = host + "?temp=" + temp;
+
+      http.begin(client, url);
+      int codigo = http.GET();
+
+      if (codigo > 0) {
+        Serial.println("✅ Temperatura enviada: " + temp + " | Código HTTP: " + String(codigo));
+      } else {
+        Serial.println("❌ Erro HTTP: " + http.errorToString(codigo));
+      }
+
+      http.end();
+    } else {
+      Serial.println("⚠️ Wi-Fi desconectado.");
+    }
+  }
+}
